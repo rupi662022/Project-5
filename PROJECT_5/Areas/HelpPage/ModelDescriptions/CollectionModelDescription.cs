0588afd119f66fe3274cb5ef315b952732970932
ashkeitay <ashke.itay@gmail.com>
2022-03-28 18:27:04 +0300

    Create

---
+++ PROJECT_5/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace PROJECT_5.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ PROJECT_5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PROJECT_5.WebApiApplication" Language="C#" %>
