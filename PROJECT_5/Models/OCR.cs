ca48f397f3df56debcac08947c9e0a60238fce0d
GuyHerman <95701922+rupi662022@users.noreply.github.com>
2022-03-30 14:48:08 +0300

    1

--- PROJECT_5/Models/OCR.cs
+++ PROJECT_5/Models/OCR.cs
@@ -1,165 +1,81 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Web;
-//using PROJECT_5.Models.DAL;
-////using IronOcr;
-//using System.Text.RegularExpressions;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using IronOcr;
+using System.Text.RegularExpressions;
 
-//namespace PROJECT_5.Models
-//{
-//    public class OCR
-//    {
-//        Regex ContRegex = new Regex("[A-Z]{4}\\d{7}");
-//        string ContNum = "";
-//        List<string> FileName = new List<string>();
-//        List<string> Containers = new List<string>();
-//        List<string> NonFilterdContainers = new List<string>();
+namespace PROJECT_5.Models
+{
+    public class OCR
+    {
+        Regex ContRegex = new Regex("[A-Z]{4}\\d{7}");
+        string ContNum = "";
+        List<string> FileName = new List<string>();
+        List<string> Containers = new List<string>();
+        List<string> NonFilterdContainers = new List<string>();
 
 
-//        public OCR()
-//        {
-
-//TEST
-
-//        }
-
-//        public string getContNumbs(string filename)
-//        {
-//            //FileName.Add("C:\\Users\\USER\\Downloads\\"+ filename);
-
-//            var Ocr = new IronTesseract();
-//            Ocr.Language = OcrLanguage.HebrewBest;
-//            Ocr.AddSecondaryLanguage(OcrLanguage.EnglishBest);
-//            var Input = new OcrInput("C:\\Users\\USER\\Downloads\\" + filename);
-//            Input.Deskew();
-//            //Input.MinimumDPI = 250;
-//            //Input.TargetDPI = 300;
-//            Input.DeNoise();
-//            var Result = Ocr.Read(Input);
-//            Console.OutputEncoding = Encoding.GetEncoding("Windows-1255");
+        public OCR()
+        {
 
 
 
-//            //Regex ContRegex = new Regex("^[A-Z]{4}\\d{7}");
+        }
 
-//            for (int i = 0; i < Result.Words.Length; i++)
-//            {
+        public string getContNumbs(string filename)
+        {
+            //FileName.Add("C:\\Users\\USER\\Downloads\\"+ filename);
 
-//                //Console.Write(Result.Words[i].ToString());
-
-//                Match m = ContRegex.Match(Result.Words[i].ToString());
-
-//                if (m.Success)
-//                {
-//                    if (m.Value[3] == 'U')
-//                    {
-//                        ContNum = m.Value;
-
-
-
-//                        //if (Containers.Contains(ContNum) != true)
-//                        //{
-//                        //    Containers.Add(ContNum);//afer that we need to return this list straight to the Haifa port API
-//                        //    NonFilterdContainers.Add(Result.Words[i].ToString());
-//                        //}
-//                    }
-
-
-//                }
-
-//            }
+            var Ocr = new IronTesseract();
+            Ocr.Language = OcrLanguage.HebrewBest;
+            Ocr.AddSecondaryLanguage(OcrLanguage.EnglishBest);
+            var Input = new OcrInput("C:\\Users\\USER\\Downloads\\" + filename);
+            Input.Deskew();
+            //Input.MinimumDPI = 250;
+            //Input.TargetDPI = 300;
+            Input.DeNoise();
+            var Result = Ocr.Read(Input);
+            Console.OutputEncoding = Encoding.GetEncoding("Windows-1255");
 
 
 
-//            return ContNum;
+            //Regex ContRegex = new Regex("^[A-Z]{4}\\d{7}");
 
-//        }
+            for (int i = 0; i < Result.Words.Length; i++)
+            {
 
+                //Console.Write(Result.Words[i].ToString());
 
-//        //    }
-//    }
+                Match m = ContRegex.Match(Result.Words[i].ToString());
 
-
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using IronOcr;
-//using System.Text.RegularExpressions;
-//namespace ConsoleApp9
-
-//    {
-//        class Program
-//        {
-//            static void Main(string[] args)
-//            {
-//                Regex ContRegex = new Regex("[A-Z]{4}\\d{7}");
-//                string ContNum = "";
-//                List<string> FileName = new List<string>();
-//                List<string> Containers = new List<string>();
-//                List<string> NonFilterdContainers = new List<string>();
-
-
-//                for (int i = 0; i < 5; i++)///Adding the files to the list, ideally we need to send here the files names from the Gmail api
-//                {
-//                    FileName.Add("C:\\temp\\GatePass" + i + ".pdf");
-//                }
-
-
-//                foreach (string x in FileName)
-//                {
-
-//                    var Ocr = new IronTesseract();
-//                    Ocr.Language = OcrLanguage.HebrewBest;
-//                    Ocr.AddSecondaryLanguage(OcrLanguage.EnglishBest);
-//                    var Input = new OcrInput(x);
-//                    Input.Deskew();
-//                    //Input.MinimumDPI = 250;
-//                    //Input.TargetDPI = 300;
-//                    Input.DeNoise();
-//                    var Result = Ocr.Read(Input);
-//                    Console.OutputEncoding = Encoding.GetEncoding("Windows-1255");
+                if (m.Success)
+                {
+                    if (m.Value[3] == 'U')
+                    {
+                        ContNum = m.Value;
 
 
 
-//                    //Regex ContRegex = new Regex("^[A-Z]{4}\\d{7}");
-
-//                    for (int i = 0; i < Result.Words.Length; i++)
-//                    {
-
-//                        //Console.Write(Result.Words[i].ToString());
-
-//                        Match m = ContRegex.Match(Result.Words[i].ToString());
-
-//                        if (m.Success)
-//                        {
-//                            if (m.Value[3] == 'U')
-//                            {
-//                                ContNum = m.Value;
-
-//                                Console.WriteLine(ContNum);
+                        //if (Containers.Contains(ContNum) != true)
+                        //{
+                        //    Containers.Add(ContNum);//afer that we need to return this list straight to the Haifa port API
+                        //    NonFilterdContainers.Add(Result.Words[i].ToString());
+                        //}
+                    }
 
 
-//                                if (Containers.Contains(ContNum) != true)
-//                                {
-//                                    Containers.Add(ContNum);//afer that we need to return this list straight to the Haifa port API
-//                                    NonFilterdContainers.Add(Result.Words[i].ToString());
-//                                }
-//                            }
+                }
+
+            }
 
 
-//                        }
 
-//                    }
-//                }
+            return ContNum;
+
+        }
 
 
-//                Console.ReadLine();
-
-//            }
-//        }
-//    }
-
+    }
+}
