095007fb50030575c6ad52faf0bd99f5a370bfec
ShimronR <lolshimi99@gmail.com>
2022-03-31 15:48:28 +0300

    Fixed gatepass page

---
+++ PROJECT_5/Models/GmailAPI.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PROJECT_5.Models
+{
+    public class GmailAPI
+    {
+        string CLIENT_ID;
+        string API_KEY;
+
+        public GmailAPI()
+        {
+
+
+
+        }
+
+        public void InitAPI()
+        {
+         this.CLIENT_ID = "444171297629-8shug2a3urepctfatbqcmuohpsvaog7k.apps.googleusercontent.com";
+         this.API_KEY = "AIzaSyDSdMaEiTVy_P0IBuvRWrEZSdnHJzmGJH8";
+           
+
+
+        }
+
+
+    }
+}
