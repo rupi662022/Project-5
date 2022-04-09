ff8d655ebf3254bdfc6c4d640fffd90994171402
ShimronR <lolshimi99@gmail.com>
2022-04-09 16:17:16 +0300

    API attempts

--- PROJECT_5/Indian Toturial Classes/Gmail.cs
+++ PROJECT_5/Indian Toturial Classes/Gmail.cs
@@ -8,18 +8,11 @@ namespace PROJECT_5.Models
 {
     public class Gmail
     {
-
         public string From { get; set; }
-
         public string To { get; set; }
-
         public string Body { get; set; }
-
         public DateTime MailDateTime { get; set; }
-
         public List<string> Attachments { get; set; }
-
         public string MsgID { get; set; }
-
     }
 }
